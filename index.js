b96878a1b39fd74e940e2a7f3161f5338628aee1
Bamidelex1132 <dikkotoheeb@gmail.com>
2025-07-17 14:18:34 -0700

    update-index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth');
-require('dotenv').config();
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,13 +21,26 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Routes
 app.use('/', authRoutes);
 
+
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URI)
+     .then(() => {
+         console.log('MongoDB connected');
+         app.listen(PORT, () => {
+             console.log(`Server running on http://localhost:${PORT}`);
+         });
+     })
+     .catch(err => {
+        console.error('DB connection error:', err);
+     });
 // Connect to MongoDB and start server
-mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true
-}).then(() => {
-console.log('MongoDB connected');
-app.listen(PORT, () => {
-console.log(`Server running on http://localhost:${PORT}`);
-});
-}).catch(err => console.error('DB connection error:', err));
+//mongoose.connect(process.env.MONGO_URI, {
+//useNewUrlParser: true,
+//useUnifiedTopology: true
+//}).then(() => {
+//console.log('MongoDB connected');
+//app.listen(PORT, () => {
+//console.log(`Server running on http://localhost:${PORT}`);
+//});
+//}).catch(err => console.error('DB connection error:', err));
