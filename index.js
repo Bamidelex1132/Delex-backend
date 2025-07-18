4c1fa35de48c5520e24e1dd9b34752166fbc6dee
Bamidelex1132 <dikkotoheeb@gmail.com>
2025-07-18 14:29:37 -0700

    edit:server

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/auth', authRoutes) // Use '/auth' as base route
 // Start server
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 // Optional: Test Supabase query
